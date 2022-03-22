aaa8c6c8b7555c9b1abe655787e076c7e4e7e76f
henrietta_21 <haryormeedhey@gmail.com>
2022-03-22 17:17:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import MovieList from './components/MovieList';
 import Header from './components/Header';
