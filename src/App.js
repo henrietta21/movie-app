a74bf740b9f3c0174cb16864b6e32e933171a577
henrietta_21 <haryormeedhey@gmail.com>
2022-03-22 11:16:42 +0100

    initialise project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import MovieList from './components/MovieList';
 import Header from './components/Header';
@@ -11,30 +12,31 @@ function App() {
   const [movies, setMovies] = useState([])
   const [searchInput, setSearchInput] = useState('')
 
-const getMovieRequest = async (searchInput) => {
-  const url=`http://www.omdbapi.com/?s=${searchInput}&apikey=da88be3`
-  const response = await fetch(url)
-  const responseJson = await response.json()
+  const getMovieRequest = async (searchInput) => {
+    const url=`http://www.omdbapi.com/?s=${searchInput}&apikey=da88be3`
+    const response = await fetch(url)
+    const responseJson = await response.json()
 
- if(responseJson.Search){
-  setMovies(responseJson.Search)
- }
-}
+    if(responseJson.Search){
+      setMovies(responseJson.Search)
+    }
+    }
 
-useEffect(() =>{
-  getMovieRequest(searchInput)
-},[searchInput])
 
-const styles={
-  searchContainer:{
-    padding:'63px 57px 48px 77px',
-    display:'block'
-  },
+  useEffect(() =>{
+    getMovieRequest(searchInput)
+  },[searchInput])
 
-  movieContainer:{
-    paddingLeft:'67px'
+  const styles={
+    searchContainer:{
+      padding:'63px 57px 48px 77px',
+      display:'block'
+    },
+
+    movieContainer:{
+      paddingLeft:'67px'
+    }
   }
-}
 
   return (
     <div className="App">
